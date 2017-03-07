891a6a17f0e838ba64af3d0d7373fb04cef3fa50
Arthur Tofani <gramofone@gmail.com>
2017-03-07 16:20:55 -0300

    fixed whitening

--- echoprint-pycodegen/test_whitening.py
+++ echoprint-pycodegen/test_whitening.py
@@ -15,3 +15,19 @@ class TestWhitening(unittest.TestCase):
   def test_compute_block(self):
     block = self.audio.readframes(whitening.BLOCK_LENGTH)
     self.obj.compute_block(block)
+
+  def test_calculate_block_autocorrelation(self):
+    block = self.audio.readframes(whitening.BLOCK_LENGTH)
+    while block != '':
+      r = self.obj.calculate_block_autocorrelation(block)
+      self.assertEqual(len(r), self.obj.P)
+      self.assertNotEqual(r[0], 0)
+      block = self.audio.readframes(whitening.BLOCK_LENGTH)
+
+  def test_calculate_new_filter_coefficients(self):
+      block = self.audio.readframes(whitening.BLOCK_LENGTH)
+      self.obj.R = self.obj.calculate_block_autocorrelation(block)
+      E = self.obj.calculate_new_filter_coefficients()
+
+  def test_calculate_new_output(self):
+    print("todo")
--- echoprint-pycodegen/whitening.py
+++ echoprint-pycodegen/whitening.py
@@ -1,4 +1,5 @@
 import numpy
+import struct
 import array
 import wave
 import pyaudio
@@ -13,14 +14,13 @@ class Whitening:
     # TODO: isso aqui não dá pra sempre puxar direto do audio?
     # audio = wave.open("../samples/vida_marvada_44100.wav")
     self.audio = audio
-    self.samples = audio.getnframes()
     self.num_samples = audio.getnframes()
 
     self.P = 40
     self.alpha = 1.0/T
     self.x0 = [0.0] * self.P
     self.ai = [None] * self.P
-    self._whitened = [None] * self.num_samples
+    self.whitened = [None] * self.num_samples
 
 
   def compute(self):
@@ -33,26 +33,14 @@ class Whitening:
 
   def compute_block(self, block_bytes):
     block = array.array('h', block_bytes)
-    self.R = self.calculate_block_autocorration(block)
-    E = self.R[0]
-    for i in range(self.P):
-      sumalphaR = 0.0;
-      for j in range(i):
-        sumalphaR += self.ai[j] * self.R[i-j]
-      ki = (self.R[i] - sumalphaR)/E
-      self.ai[i] = ki
-      for j in range(1, i//2): # is this actually the same??
-        aj = self.ai[j]
-        aimj = self.ai[i-j]
-        self.ai[j] = aj - ki * aimj
-        self.ai[i-j] = aimj - ki * aj
-      E = (1 - ki * ki) * E
-      print(E)
+    self.R = self.calculate_block_autocorrelation(block)
+    E = self.calculate_new_filter_coefficients()
+    self.calculate_new_output(block)
       # getsampwidth()https://gist.github.com/rpls/3760188
       # here comes the magic for each block
     return
 
-  def calculate_block_autocorration(self, block):
+  def calculate_block_autocorrelation(self, block):
     r = [0.0] * self.P
     r[0] = 0.001
     for i in range(self.P):
@@ -62,6 +50,41 @@ class Whitening:
       r[i] += self.alpha * (acc - r[i])
     return r
 
-  def new_filter_coefficients(self):
-    return
-    # ver rlevinson
+  def calculate_new_filter_coefficients(self):
+    # ver rlevinson@
+    E = self.R[0]
+    for i in range(1, self.P):
+      sumalphaR = 0.0
+      for j in range(1, i-1):
+        sumalphaR += self.ai[j] * self.R[i-j]
+      ki = (self.R[i] - sumalphaR)/E
+      self.ai[i] = ki
+      for j in range(1, i//2): # is this actually the same??
+        aj = self.ai[j]
+        aimj = self.ai[i-j]
+        self.ai[j] = aj - ki * aimj
+        self.ai[i-j] = aimj - ki * aj
+      E = (1 - ki * ki) * E
+    return E, self.ai
+
+  def calculate_new_output(self, block):
+    for i in range(len(block)-1):
+      acc = block[i]
+      minip = min(i, self.P)
+      for j in range(i+1, self.P):
+        acc -= self.ai[j]*self.x0[self.P + i-j]
+      for j in range(1, minip):
+        acc -= self.ai[j]*block[i-j]
+      self.whitened[i] = acc
+    for i in range(self.P):
+      self.x0[i] = block[len(block)-1-self.P+i]
+    return self.whitened
+
+  def save(self, filename):
+    output = wave.open(filename, 'w')
+    output.setnchannels(self.audio.getnchannels())
+    output.setsampwidth(self.audio.getsampwidth())
+    output.setframerate(self.audio.getframerate())
+    for value in range(0, len(self.whitened)-1):
+      packed_value = struct.pack('h', value)
+      output.writeframes(packed_value)
