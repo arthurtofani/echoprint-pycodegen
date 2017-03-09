31fc9c00a117d1211a21f34bcae947e15f9b165c
Arthur Tofani <gramofone@gmail.com>
2017-03-09 12:52:33 -0300

    whitened class working

--- echoprint-pycodegen/whitening.py
+++ echoprint-pycodegen/whitening.py
@@ -1,3 +1,13 @@
+##
+## "To improve robustness against varyiable OTA channel
+## characteristics, the signal is “whitened” prior to the analysis
+## above. A 40-pole LPC filter is estimated from the autocorrelation
+## of 1 sec blocks of the signal, smoothed with an 8
+## sec decay constant. The inverse (FIR) filter is applied to the
+## signal to achieve whitening."
+
+## checar de usar audiolazy - http://pythonhosted.org/audiolazy/lazy_lpc.html
+
 import numpy
 import scipy.io.wavfile as wavfile
 import struct
@@ -8,6 +18,7 @@ from spectrum import LEVINSON
 
 BLOCK_LENGTH = 10000
 T = 8
+ALPHA = 1.0/T
 
 class Whitening:
   def __init__(self, audio):
@@ -17,14 +28,15 @@ class Whitening:
 
     self.audio = audio
     self.sample_rate = audio[0]
-    self.samples = audio[1]
+    self.samples = list(map(lambda x: x / 32768.0, audio[1]))
     self.num_samples = len(audio[1])
 
     # init parameters and coeficients
-    self.P = 40
-    self.alpha = 1.0/T
-    self.x0 = [0.0] * self.P
-    self.ai = [None] * self.P
+    self._p = 40
+    self.R = [0.0] * (self._p+1)
+    self.R[0] = 0.001
+    self.x0 = [0.0] * (self._p+1)
+    self.ai = [None] * (self._p+1)
     self.whitened = [None] * self.num_samples
 
 
@@ -35,56 +47,53 @@ class Whitening:
   def compute_block(self, start):
     print(start)
     block = self.samples[start:(BLOCK_LENGTH+start):]
-    self.R = self.calculate_block_autocorrelation(block)
-    E = self.calculate_new_filter_coefficients()
-    self.calculate_new_output(block)
+    block_size = len(block)
+    self.calculate_block_autocorrelation(start, block_size)
+    self.calculate_new_filter_coefficients()
+    self.calculate_new_output(start, block_size)
     return
 
-  def calculate_block_autocorrelation(self, block):
-    r = [0.0] * self.P
-    r[0] = 0.001
-    for i in range(self.P):
+  def calculate_block_autocorrelation(self, start, block_size):
+    for i in range(self._p+1):
       acc = 0
-      for j in range(len(block)):
-        acc += block[j] * block[j-i]
-      r[i] += self.alpha * (acc - r[i])
-    return r
+      for j in range(i, block_size):
+        acc += self.samples[j+start] * self.samples[j-i+start]
+      self.R[i] += ALPHA * (acc - self.R[i])
 
   def calculate_new_filter_coefficients(self):
     # ver rlevinson@
     E = self.R[0]
-    for i in range(1, self.P):
+    for i in range(1, self._p + 1):
       sumalphaR = 0.0
-      for j in range(1, i-1):
+      for j in range(1, i):
         sumalphaR += self.ai[j] * self.R[i-j]
       ki = (self.R[i] - sumalphaR)/E
       self.ai[i] = ki
-      for j in range(1, i//2): # is this actually the same??
+      for j in range(1, (i//2)+1):
         aj = self.ai[j]
         aimj = self.ai[i-j]
         self.ai[j] = aj - ki * aimj
         self.ai[i-j] = aimj - ki * aj
       E = (1 - ki * ki) * E
-    return E, self.ai
 
-  def calculate_new_output(self, block):
-    for i in range(len(block)-1):
-      acc = block[i]
-      minip = min(i, self.P)
-      for j in range(i+1, self.P):
-        acc -= self.ai[j]*self.x0[self.P + i-j]
-      for j in range(1, minip):
-        acc -= self.ai[j]*block[i-j]
-      self.whitened[i] = acc
-    for i in range(self.P):
-      self.x0[i] = block[len(block)-1-self.P+i]
-    return self.whitened
+  def calculate_new_output(self, start, block_size):
+    # calculate new output
+    for i in range(block_size):
+      acc = self.samples[i+start]
+      minip = min(i, self._p)
+      for j in range(i+1, self._p+1):
+        acc -= self.ai[j]*self.x0[self._p + i-j]
+      #for j in range(1, minip+1):
+      #  acc -= self.ai[j]*self.samples[i-j+start]
+      self.whitened[i+start] = acc
 
-  def save(self, filename):
-    output = wave.open(filename, 'w')
-    output.setnchannels(self.audio.getnchannels())
-    output.setsampwidth(self.audio.getsampwidth())
-    output.setframerate(self.audio.getframerate())
-    for value in range(0, len(self.whitened)-1):
-      packed_value = struct.pack('h', value)
-      output.writeframes(packed_value)
+    #save last few frames of input
+    for i in range(self._p + 1):
+      self.x0[i] = self.samples[block_size-1-self._p+i+start]
+
+  def save_preview(self, filename):
+    #whitened = numpy.array(self.whitened, dtype=numpy.int16)
+    w = list(map(lambda x: x * 32768.0, self.whitened))
+    whitened = numpy.array(w, dtype=numpy.int16)
+    wavfile.write(filename, 11025, whitened)
+    numpy.array(self.whitened)
--- tmp/whitening_preview.py
+++ tmp/whitening_preview.py
@@ -1,10 +1,19 @@
+import numpy as np
 import scipy.io.wavfile as wavfile
 import whitening
-audio = wavfile.read("../samples/canto-tetrico-11025Hz_mono_16-bit.wav")
+audio = wavfile.read("../samples/come_together_01.wav")
 w = whitening.Whitening(audio)
 w.compute()
-w.save("./whitened.wav")
+w.save_preview("../samples/whitened/come_together_01.wav")
+
 
 # import scipy.io.wavfile as wavfile
 # wavfile.read("../samples/canto.wav")
 # audio = wavfile.read("../samples/canto-tetrico-11025Hz_mono_16-bit.wav")
+
+#ffmpeg -i come_together.mp3 -acodec pcm_s16le -ac 1 -ar 11025 come_together_full.wav
+
+#/home/arthur/tmp/echoprint/echoprint-pycodegen/samples/come_together_full.wav
+
+
+ffmpeg -i come_together_01.wav -acodec pcm_s16le -ac 1 -ar 11025 come_together_01b.wav
